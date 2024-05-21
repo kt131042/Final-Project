f539d3a4a04a44ed345696c18490fe390bcedafd
kt131042 <kt131042@gmail.com>
2024-05-21 08:09:20 +0700

    save

--- lib/screen/calbill.dart
+++ lib/screen/calbill.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Calculator extends StatefulWidget {
@@ -8,6 +9,22 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  void createRecord() async {
+    CollectionReference bill = FirebaseFirestore.instance.collection('room');
+    CollectionReference electricityCosts =
+        bill.doc('A101').collection('electricity_costs');
+
+    Map<String, dynamic> data = {
+      'month': 'May',
+      'year': 2024,
+      'units_used': 120,
+      'cost_per_unit': 3.50,
+      'total_cost': 420
+    };
+
+    await electricityCosts.doc('2024_05').set(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +32,11 @@ class _CalculatorState extends State<Calculator> {
       body: Column(
         children: [
           const Text("data"),
-          ElevatedButton(onPressed: () {}, child: const Text("บันทึก"))
+          ElevatedButton(
+              onPressed: () {
+                createRecord();
+              },
+              child: const Text("บันทึก"))
         ],
       ),
     );
--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jittirat/screen/calbill.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -13,6 +14,18 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Calculator(),
+                ),
+              );
+            },
+            child: const Text("คำนวณค่าเช่า")),
       ),
     );
   }
